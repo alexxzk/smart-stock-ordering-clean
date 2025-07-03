245d0ad0dcb66a704a82889ff39cc5205b8b5bb1
alexxzk <vivamelbournecity@gmail.com>
2025-07-03 21:30:42 +1000

    Update env.ts

--- frontend/src/config/env.ts
+++ frontend/src/config/env.ts
@@ -2,12 +2,12 @@
 export const config = {
   apiUrl: (import.meta as any)?.env?.VITE_API_URL || 'http://localhost:8000',
   firebase: {
-    apiKey: (import.meta as any)?.env?.VITE_FIREBASE_API_KEY || "AIzaSyCaFitFM9ZbS2fjz0QJvocpGLUkzeF5AXI",
-    authDomain: (import.meta as any)?.env?.VITE_FIREBASE_AUTH_DOMAIN || "ordix-stock-ordering.firebaseapp.com",
-    projectId: (import.meta as any)?.env?.VITE_FIREBASE_PROJECT_ID || "ordix-stock-ordering",
-    storageBucket: (import.meta as any)?.env?.VITE_FIREBASE_STORAGE_BUCKET || "ordix-stock-ordering.firebasestorage.app",
-    messagingSenderId: (import.meta as any)?.env?.VITE_FIREBASE_MESSAGING_SENDER_ID || "758571654408",
-    appId: (import.meta as any)?.env?.VITE_FIREBASE_APP_ID || "1:758571654408:web:99d197679cab335a56e0bd",
-    measurementId: (import.meta as any)?.env?.VITE_FIREBASE_MEASUREMENT_ID || "G-JV7R4PZQHV"
+    apiKey: (import.meta as any)?.env?.VITE_FIREBASE_API_KEY || "AIzaSyBfPjAU2usnP6ofheyfGgZ6-NK2VuHAEAc",
+    authDomain: (import.meta as any)?.env?.VITE_FIREBASE_AUTH_DOMAIN || "ordix-65b91.firebaseapp.com",
+    projectId: (import.meta as any)?.env?.VITE_FIREBASE_PROJECT_ID || "ordix-65b91",
+    storageBucket: (import.meta as any)?.env?.VITE_FIREBASE_STORAGE_BUCKET || "ordix-65b91.firebasestorage.app",
+    messagingSenderId: (import.meta as any)?.env?.VITE_FIREBASE_MESSAGING_SENDER_ID || "480905571530",
+    appId: (import.meta as any)?.env?.VITE_FIREBASE_APP_ID || "1:480905571530:web:df9cda514086474d815833",
+    measurementId: (import.meta as any)?.env?.VITE_FIREBASE_MEASUREMENT_ID || "G-HJ70T8VWJL"
   }
 }; 
