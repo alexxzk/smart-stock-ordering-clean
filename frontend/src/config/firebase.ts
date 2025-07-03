779cf85de96790cbf6997e1f61694d3e99eb5f27
alexxzk <vivamelbournecity@gmail.com>
2025-07-03 22:09:15 +1000

    Update firebase.ts

--- frontend/src/config/firebase.ts
+++ frontend/src/config/firebase.ts
@@ -3,6 +3,8 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { config } from './env';
 
+import { config } from './env'; console.log('FIREBASE CONFIG:', config.firebase); // <-- Add this line
+
 // Use the config object for all env variables
 const firebaseConfig = config.firebase;
 
